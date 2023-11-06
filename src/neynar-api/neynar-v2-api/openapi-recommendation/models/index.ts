e7ef31bfe2473d801c099f00397a8376ce1c6c50
Shreyaschorge <shreyas@neynar.com>
2023-11-06 11:44:54 +0530

    Generate code for recommendation api

---
+++ src/neynar-api/neynar-v2-api/openapi-recommendation/models/index.ts
@@ -0,0 +1,7 @@
+export * from './active-status';
+export * from './error-res';
+export * from './fetch-relevant-mints200-response';
+export * from './relevant-mint';
+export * from './user';
+export * from './user-profile';
+export * from './user-profile-bio';
