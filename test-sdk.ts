becbf630559d97f036064ad8eeb0bb2d772aabe8
Shreyaschorge <shreyas@neynar.com>
2023-12-20 21:52:33 +0530

    Remove test-sdk file

--- test-sdk.ts
+++
@@ -1,7 +0,0 @@
-import { NeynarAPIClient } from './build'
-
-const client = new NeynarAPIClient('ntest')
-
-client.searchChannels("ux").then((response) => {
-  console.log("Search Results:", response);
-});
