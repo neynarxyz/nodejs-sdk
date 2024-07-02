6e3e2d3f15795ad3fb5f484247e49b6907a16b7d
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-07-02 19:16:43 -0400

    clean up subscriber responses

--- src/neynar-api/v2/openapi-farcaster/models/tier.ts
+++ src/neynar-api/v2/openapi-farcaster/models/tier.ts
@@ -31,57 +31,9 @@ export interface Tier {
     'id'?: number;
     /**
      * 
-     * @type {string}
-     * @memberof Tier
-     */
-    'title'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof Tier
-     */
-    'art_url'?: string;
-    /**
-     * 
      * @type {TierPrice}
      * @memberof Tier
      */
     'price'?: TierPrice;
-    /**
-     * 
-     * @type {number}
-     * @memberof Tier
-     */
-    'max_commitment_seconds'?: number;
-    /**
-     * 
-     * @type {string}
-     * @memberof Tier
-     */
-    'start_timestamp'?: string | null;
-    /**
-     * 
-     * @type {string}
-     * @memberof Tier
-     */
-    'end_timestamp'?: string | null;
-    /**
-     * 
-     * @type {number}
-     * @memberof Tier
-     */
-    'reward_basis_points'?: number;
-    /**
-     * 
-     * @type {boolean}
-     * @memberof Tier
-     */
-    'paused'?: boolean;
-    /**
-     * 
-     * @type {boolean}
-     * @memberof Tier
-     */
-    'transferrable'?: boolean;
 }
 
