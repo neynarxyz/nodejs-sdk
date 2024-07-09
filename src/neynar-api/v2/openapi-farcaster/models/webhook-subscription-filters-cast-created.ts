a624eb8d4c1c82ca2b10747291b781ffcae77035
Shreyaschorge <shreyas@neynar.com>
2024-07-10 01:40:32 +0530

    generated code

--- src/neynar-api/v2/openapi-farcaster/models/webhook-subscription-filters-cast-created.ts
+++ src/neynar-api/v2/openapi-farcaster/models/webhook-subscription-filters-cast-created.ts
@@ -57,6 +57,12 @@ export interface WebhookSubscriptionFiltersCastCreated {
      */
     'text'?: string;
     /**
+     * Exclude casts that matches the text regex from these authors. ** Use case: **   If you want to reply \"$degen\" from a bot, and the text you are looking for is also \"$degen\" (i.e. text regex is set to (?i)\\\\$degen).   You need to add the bot\'s fid to this list to avoid replying to the bot\'s cast. This will prevent infinite loops of replying to each other. 
+     * @type {Array<number>}
+     * @memberof WebhookSubscriptionFiltersCastCreated
+     */
+    'exclude_text_fids'?: Array<number>;
+    /**
      * Regex pattern to match the embeded_url (key embeds) of the cast. **Note:**  1) Regex must be parsed by Go\'s RE2 engine (Test your expression here: https://www.lddgo.net/en/string/golangregex) 2) Use backslashes to escape special characters. For example: \\\\b(farcaster|neynar)\\\\b should be written as \\\\\\\\b(farcaster|neynar)\\\\\\\\b 
      * @type {string}
      * @memberof WebhookSubscriptionFiltersCastCreated
