30d786e08071239ee66b7c3ef1b5a22bf7810b28
Shreyaschorge <shreyas@neynar.com>
2024-11-12 21:04:15 +0530

    Add starter code and update path

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+const semver = require("semver");
+const requiredVersion = require("../package.json").engines.node;
+
+// Check Node.js version before requiring/doing anything else
+if (!semver.satisfies(process.version, requiredVersion)) {
+  console.error(
+    `Unsupported Node.js version! Your version: ${process.version}. Required version: ${requiredVersion}.`
+  );
+  process.exit(1);
+}
+
+export * from "./clients";
+export * from "./utils";
+export { Configuration } from "./clients/configuration";
